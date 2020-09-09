42fc64fb89440b650b70be82eaa5cc67611da023
yash.vyas95 <yash.vyas95@gmail.com>
2020-09-08 20:53:46 -0400

    LeetCode-13

--- src/com/LeetCode/Solutions/Main.java
+++ src/com/LeetCode/Solutions/Main.java
@@ -1,8 +1,12 @@
 package com.LeetCode.Solutions;
 
+import com.LeetCode.Solutions.NumeralsRoman.NumeralsRoman;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        System.out.println(NumeralsRoman.RomanToInt("MDLXX"));
+
     }
 }
--- src/com/LeetCode/Solutions/NumeralsRoman/NumeralsRoman.java
+++ src/com/LeetCode/Solutions/NumeralsRoman/NumeralsRoman.java
@@ -1,23 +1,100 @@
 package com.LeetCode.Solutions.NumeralsRoman;
 
+import com.sun.org.apache.xpath.internal.operations.Bool;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class NumeralsRoman {
     public static int RomanToInt(String s){
 
-        int[] values = {1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000};
-        String[] container = {"I", "IV", "V", "IX", "X", "XL", "L", "XC", "C", "CD", "D", "CM", "M"};
+        int[] values = {1, 5, 10, 50, 100, 500, 1000};
+        ArrayList<Character> Big_Container = new ArrayList<Character>(Arrays.asList('I','V','X','L','C','D','M'));
+        int total_value = 0;
+        int valueIndex = -1;
+        for(int index = 0 ; index < s.length() ; index++){
+            Character c = s.charAt(index);
+            if(c == Big_Container.get(0) || c == Big_Container.get(2) || c == Big_Container.get(4))
+            {
+                if(c == 'I' && index!= s.length()-1)
+                {
+                        if (s.charAt(index + 1) == 'V') {
+                            total_value += 4;
+                            index++;
+                        } else if (s.charAt(index + 1) == 'X') {
+                            total_value += 9;
+                            index++;
+                        }
+                        else{
+                            total_value+=values[get_Index(c)];
+                        }
+                }
+                else if(c == 'X' && index!= s.length()-1){
+                    if(s.charAt(index+1) == 'L' && index != s.length()-1){
+                        total_value+=40;
+                        index++;
+                    }
+                    else if(s.charAt(index+1) == 'C' && index != s.length()-1){
+                        total_value+=90;
+                        index++;
+                    }
+                    else{
+                        total_value+= values[get_Index(c)];
+                    }
+                }
+                else if(c == 'C' && index!= s.length()-1){
+                    if(s.charAt(index+1) == 'D' && index != s.length()-1){
+                        total_value+=400;
+                        index++;
+                    }
+                    else if(s.charAt(index+1) == 'M' && index != s.length()-1){
+                        total_value+=900;
+                        index++;
+                    }
+                    else{
+                        total_value+= values[get_Index(c)];
+                    }
+                }
+                else{
+                    total_value+=values[get_Index(c)];
+                }
 
-        boolean condition = true;
-        int index = 0;
-        String result = "";
+            }
+            else{
+                total_value+=values[get_Index(c)];
+            }
+        }
 
-     //   for(int i = 0 ; i < s.length() ; i++){
-           // Char c = s.charAt(i);
-            //if(c == 'I'){}
-           // else if(c=='X'){}
-           // else if(c=='C'){}
-           // else{}
-       // }
 
-        return 0;
+        return total_value;
+    }
+    public static int get_Index(Character c){
+        int index = -1;
+        switch(c){
+            case 'I':
+                index = 0;
+                break;
+            case 'V':
+                index = 1;
+                break;
+            case 'X':
+                index = 2;
+                break;
+            case 'L':
+                index = 3;
+                break;
+            case 'C':
+                index = 4;
+                break;
+            case 'D':
+                index = 5;
+                break;
+            case 'M':
+                index = 6;
+                break;
+
+        }
+        return index;
     }
 }
