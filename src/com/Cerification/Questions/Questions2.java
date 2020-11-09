2565bc4fe600798f33c460efe1273f0d6b433e32
yash.vyas95 <yash.vyas95@gmail.com>
2020-11-09 16:59:41 -0500

    Concepts+Questions IN COMMENT WITH DIFFERENT ENTRY POINTS
    Questions - 2 Also Added

--- src/com/Cerification/Questions/Questions2.java
+++ src/com/Cerification/Questions/Questions2.java
@@ -1,10 +1,16 @@
 package com.Cerification.Questions;
 
-import groovy.json.internal.IO;
-
 import java.io.EOFException;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.time.Period;
+import java.time.Period.*;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Predicate;
+import java.time.LocalDate;
 
 public class Questions2 {
 
@@ -13,6 +19,92 @@ public class Questions2 {
     }
 }
 
+class e24{
+    public void method(){
+        LocalDate d1 = LocalDate.of(2017,1,1);
+        Period period = Period.ofDays(1);
+        DateTimeFormatter f = DateTimeFormatter.ofPattern("MM-dd-YYYY");
+        System.out.println(f.format(d1.minus(period)));
+    }
+}
+
+
+class e23 {
+    public void method() {
+        Period period1 = Period.ofWeeks(1).ofDays(3);
+        Period period2 = Period.ofDays(10);
+        System.out.println(period1.equals(period2));
+        System.out.println(period1.toString());
+        System.out.println(period2.toString());
+    }
+}
+
+class e22{
+    public void method(){
+        Predicate<String> dash = c-> c.startsWith("-");
+        System.out.println(dash.test("-"));
+        //Predicate<Integer> p = {return i->i!=0}
+        LocalDate d = LocalDate.of(2012,12,25);
+        d.plusDays(-1);
+        System.out.println(d.getDayOfMonth());
+    }
+}
+
+
+
+
+class e21{
+    public void method(){
+        //Predicate<String> pred = (String s) -> true;
+        //System.out.println(pred);
+        String one = new String("1");
+        String two = one.concat("2");
+        System.out.println(one==two);
+        System.out.println(two);
+    }
+}
+class e20{
+    public void method() {
+        String teams = new String("694");
+        teams.concat(" 1155");
+        teams.concat(" 2255");
+        System.out.println(teams);
+        String[] array = {"Neo", "Boy"};
+        //List<String> array2 = new Arrays.asList(array);
+       // array2.set("art");
+    }
+}
+class e19{
+    interface Target{
+        boolean needToAim(double angle);
+    }
+    static void prepare(double angle, Target t){
+        boolean ready = t.needToAim(angle);
+        System.out.println(ready);
+    }
+
+    //e19.prepare(45,d->d>5||d<-5);
+}
+class e18{
+    public void method(){
+        List<String> museum = new ArrayList<>(1);
+        museum.add("Natural Science");
+        museum.add("2 Addition");
+        museum.add("3 Addition");
+        museum.remove(2);
+        System.out.println(museum);
+    }
+}
+class e17{
+    public void method(){
+        System.out.println("1-- "+new StringBuilder().append("clown"));
+        System.out.println("2-- "+new StringBuilder("clown"));
+        System.out.println("3-- "+new StringBuilder("cl").insert(2,"own"));
+        System.out.println("4-- "+new StringBuilder("cl").insert(2,"own"));
+       // StringBuilder sb = new StringBuilder("Clown").insert(sb.length(),"robots");
+
+    }
+}
 class e16{
     public void method(){
         try{}
@@ -22,9 +114,6 @@ class e16{
     }
 
 }
-
-
-
 class e15{
     class DragonException extends Exception{}
     class Lair{
