972bb1f873ee0d134c279bc8eecd16190ae7e72b
yashvays95 <yashvyas95@gmail.com>
2022-05-30 19:07:24 -0400

    LeetCode - Practice Problems

---
+++ src/com/LeetCode/Solutions/passwordStrength/solution.java
@@ -0,0 +1,33 @@
+package com.LeetCode.Solutions.passwordStrength;
+
+public class solution {
+
+
+    /*
+     * Complete the 'findPasswordStrength' function below.
+     *
+     * The function is expected to return a LONG_INTEGER.
+     * The function accepts STRING password as parameter.
+     */
+    // TIME COMPLEXITY : (O)(N)
+    public static long findPasswordStrength(String password) {
+
+        long strength = 0;
+        long cur = 0;
+        long[] pos = new long[26];
+        for (int i = 0; i < password.length(); i++) {
+            cur += i + 1 - pos[password.charAt(i) - 'a'];
+            pos[password.charAt(i) - 'a'] = i + 1;
+            strength += cur;
+        }
+        return strength;
+    }
+
+
+    public static void main(String[] args) {
+        String s = "good";
+        System.out.println(findPasswordStrength(s));
+    }
+
+
+}
